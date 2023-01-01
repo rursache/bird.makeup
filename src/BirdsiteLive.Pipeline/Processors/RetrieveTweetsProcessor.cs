f2c0d559169ef36c85b4b2382c6c46ce697b3e9b
Vincent Cloutier <vincent@cloutier.co>
2023-01-01 11:09:40 -0500

    version updates

--- src/BirdsiteLive.Pipeline/Processors/RetrieveTweetsProcessor.cs
+++ src/BirdsiteLive.Pipeline/Processors/RetrieveTweetsProcessor.cs
@@ -60,7 +60,7 @@ namespace BirdsiteLive.Pipeline.Processors
                     await _twitterUserDal.UpdateTwitterUserAsync(user.Id, user.LastTweetPostedId, user.LastTweetSynchronizedForAllFollowersId, user.FetchingErrorCount, now);
                 }
                 
-                await Task.Delay(750);
+                await Task.Delay(350);
             }
 
             return usersWtTweets.ToArray();
