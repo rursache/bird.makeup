72e4d785f7b3b29c6ea3dba7cb9ea0025dccdb3d
Vincent Cloutier <vincent@cloutier.co>
2023-08-30 17:36:12 -0400

    added port settings to db

--- src/BirdsiteLive.Common/Settings/DbSettings.cs
+++ src/BirdsiteLive.Common/Settings/DbSettings.cs
@@ -4,6 +4,7 @@
     {
         public string Type { get; set; }
         public string Host { get; set; }
+        public string Port { get; set; }
         public string Name { get; set; }
         public string User { get; set; }
         public string Password { get; set; }
