760969c11968dc9523c36de1011a919969bc6617
Radu Ursache <3800336+rursache@users.noreply.github.com>
2023-09-04 20:17:43 +0300

    Update TwitterTweetsService.cs

--- src/BirdsiteLive.Twitter/TwitterTweetsService.cs
+++ src/BirdsiteLive.Twitter/TwitterTweetsService.cs
@@ -96,8 +96,9 @@ namespace BirdsiteLive.Twitter
 
         public async Task<ExtractedTweet[]> GetTimelineAsync(SyncTwitterUser user, long fromTweetId = -1)
         {
-            if (user.Followers > 15)
-                return await TweetFromNitter(user, fromTweetId);
+            await Task.Delay(1000);
+            
+            return await TweetFromNitter(user, fromTweetId);
 
             var client = await _twitterAuthenticationInitializer.MakeHttpClient();
 
