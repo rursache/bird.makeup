668e283d4b81d0da9ee9f5635860e941f915f34a
Radu Ursache <3800336+rursache@users.noreply.github.com>
2024-02-27 17:34:06 +0200

    Update Image.cs
    ActivityPub type for icon and image should be 'Image', not 'image'?
    
    https://gitlab.com/davinkevin.fr/projects/dotmakeup/bird.makeup/-/commit/e330aceda32cd32fbceb4b71abfb93506c5cf2ce

--- src/BirdsiteLive.ActivityPub/Models/Image.cs
+++ src/BirdsiteLive.ActivityPub/Models/Image.cs
@@ -2,7 +2,7 @@
 {
     public class Image
     {
-        public string type { get; set; } = "image";
+        public string type { get; set; } = "Image";
         public string mediaType { get; set; }
         public string url { get; set; }
     }
